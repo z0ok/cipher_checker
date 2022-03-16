f234d7c9cfcc35c4b8e68305ec62baaff55b6d0e
z0ok <zzzoookkk@yandex.ru>
2022-03-16 15:05:33 +0300

    Add output directory setting

--- checker.py
+++ checker.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import argparse
 import datetime
+import os
 
 ### ============================
 class scaner:
@@ -30,6 +31,7 @@ class scaner:
 'AES128-SHA',
 'PSK-AES128-CBC-SHA']
         self.verbose = False
+        self.output = './'
 
     def show_details(self):
         try:
@@ -37,6 +39,7 @@ class scaner:
             for item in self.targets:
                 print('- {}'.format(item))
             print('[*] Target port:', self.port)
+            print('[*] Target path:', self.output)
             print('[*] Target ciphers:')
             for item in self.ciphers:
                 print('- {}'.format(item))
@@ -53,15 +56,15 @@ class scaner:
                         answ = self.__check_cipher(ip_item, cip_item)
                         if answ == 0:
                             if self.verbose: print(' [+]')
-                            with open('success.txt', 'a') as f_obj:
+                            with open(os.path.join(self.output, 'success.txt'), 'a') as f_obj:
                                 f_obj.write('[+] ' + datetime.datetime.utcnow().strftime('%y.%m.%d.%H:%M:%S') + ' | ' + '{} accepted {}\n'.format(ip_item, cip_item))
                         elif answ == 1:
                             if self.verbose: print(' [-]')
-                            with open('rejects.txt', 'a') as f_obj:
+                            with open(os.path.join(self.output, 'rejects.txt'), 'a') as f_obj:
                                 f_obj.write('[-] ' + datetime.datetime.utcnow().strftime('%y.%m.%d.%H:%M:%S') + ' | ' + '{} rejected {}\n'.format(ip_item, cip_item))
                         elif answ == 2:
                             if self.verbose: print(' [?]')
-                            with open('no_answ.txt', 'a') as f_obj:
+                            with open(os.path.join(self.output, 'no_answ.txt'), 'a') as f_obj:
                                 f_obj.write('[-] ' + datetime.datetime.utcnow().strftime('%y.%m.%d.%H:%M:%S') + ' | ' + '{} didn\'t answered\n'.format(ip_item, cip_item))
                     except Exception as error:
                         print('[-] Error: {}'.format(error))
@@ -74,7 +77,7 @@ class scaner:
             cmd = ['timeout','1','openssl', 's_client', '-connect', '{}:{}'.format(ip, self.port), '-cipher', cip]
             all_data = subprocess.run(cmd, capture_output=True)
             data = all_data.stdout.decode().strip() + all_data.stderr.decode().strip()
-            with open('log.txt','a') as f_obj:
+            with open(os.path.join(self.output, 'log.txt'),'a') as f_obj:
                 f_obj.write('='*32+'\n[*] ' + datetime.datetime.utcnow().strftime('%y.%m.%d.%H:%M:%S') + ' | ' + '{}\n'.format(data))
             if data == '':
                 return 2
@@ -95,6 +98,7 @@ def args():
         parser.add_argument('--ip', dest='scope_ip', action='store', help='Single IP to check')
         parser.add_argument('--port', dest='target_port', action='store', help='Port to check. Default 443')
         parser.add_argument('--ciphers', dest='ciph_file', action='store', type=argparse.FileType('r'), help='File with Ciphers. Default - SHA1.')
+        parser.add_argument('--output', dest='output', action='store', help='Output dir. Default - ./')
         parser.add_argument('--verbose', dest='verbose', action='store_true', help='Print all results')
         if len(sys.argv)==1:
             parser.print_help(sys.stderr)
@@ -106,10 +110,6 @@ def args():
         if parser.scope_file and parser.scope_ip:
             print('[-] Either scope list or 1 IP. I\'m lazy ;)')
             exit()
-        if parser.verbose:
-            scan_obj.verbose = True
-        if parser.target_port:
-            scan_obj.port = parser.target_port
         if parser.scope_file:
             scan_obj.targets = strip_targets(parser.scope_file)
         elif parser.scope_ip:
@@ -117,6 +117,15 @@ def args():
         else:
             print('[-] No target. ')
             exit()
+        if parser.verbose: scan_obj.verbose = True
+        if parser.target_port: scan_obj.port = parser.target_port
+        if parser.output: 
+            if os.path.isdir(parser.output): 
+                scan_obj.output = parser.output
+            else: 
+                print('[-] No such directory :(')
+                exit()
+            
         ### Init ciphers 
         if parser.ciph_file:
             scan_obj.ciphers = strip_targets(parser.ciph_file)
@@ -138,10 +147,10 @@ def strip_targets(target_f):
         exit()
 
 def clear_files():
-    open('log.txt', 'w')
-    open('success.txt', 'w')
-    open('no_answ.txt', 'w')
-    open('rejects.txt', 'w')
+    open(os.path.join(self.output, 'log.txt'), 'w')
+    open(os.path.join(self.output, 'success.txt'), 'w')
+    open(os.path.join(self.output, 'no_answ.txt'), 'w')
+    open(os.path.join(self.output, 'rejects.txt'), 'w')
 
 ### ============================
 if __name__ == '__main__':
